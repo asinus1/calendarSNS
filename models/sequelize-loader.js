fe207f267d33dbafb5eb1a5650fb7fc214ff6f4c
Fiction <hastur@me.com>
2021-02-07 20:11:13 +0900

    Update sequelize-loader.js
    ポスグレID/パスワード統一用。

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,16 +1,17 @@
 'use strict';
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/calendersns',
+  process.env.DATABASE_URL || 'postgres://trope:57tYp6BvyKGS35RQ@localhost:5432/calendarsns',
   {
+    dialect: 'postgres',
+    protocol: 'postgres',
     dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
+      // ssl: {
+      //   require: true,
+      //   rejectUnauthorized: false}
       }
-    }
   }
-  );
+);
 
 module.exports = {
   database: sequelize,
